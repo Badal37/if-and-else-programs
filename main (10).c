593b0998948c6f3c846577c0c4538acd0711042a
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-11-15 00:13:17 +0530

    Add files via upload

---
+++ main (10).c
@@ -0,0 +1,24 @@
+      /*To find maximum number by if else */
+#include <stdio.h>
+
+int main()
+
+{ 
+  int num1,num2;
+  
+  printf("Enter your numbers");
+  
+  scanf("%d%d",&num1,&num2);
+  
+  if(num1>num2)
+  {
+    printf("num1 is greater");
+  }
+  else
+  {
+    printf("num2 is greater");
+  }
+
+return 0;
+
+}
